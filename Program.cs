7f17aa33c7adc8f468a222bacaf0a0931d3f9560
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-20 10:46:30 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestDrivenDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // UnitTesting Aproaches !
+            // TestDrivenDevelopment - TDD -Test Driven Devlopment !
+            // Aproach --code first then code and test approach--classical approach !
+            //
+            //Test First -approach ->Test Driven development, first we seat and, first we create the architecture of the calsses, methods, 
+            // what the user will input, waht will be the output.
+            //
+
+            // The standard approach --DevelopCode first-->  then  write the unit Test, Run and succed.
+            // i e, code and tehn test approach !
+            // clean tests---Tests on the code -tests are expected to work !
+            // dirty tests- >  test that we read, and we expect they to brak the code, to find a bug !
+            // in a Big project , the dirty tests muss be more  than the clean test!
+            // Clean tests--tehy chweck the normal behaviour of the method.
+            // if we write the unit test with intention to break the code, then we will have more success.
+            // If we write the unit test with thte conseption to prove that out code works, we will have less success
+            // unit tests must find bugs ! must show the defects, unittest pays off lot !
+            // Test Driven development- helps to find design -Problems early with our code !
+
+        }
+    }
+}
